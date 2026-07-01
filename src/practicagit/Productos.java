7cf84ee9daa83c047adc8444074ba777d259b3b5
santiagoat1212 <santiago.campos.25s@utzmg.edu.mx>
2026-07-01 14:49:54 -0600

    correciones al gay

--- src/practicagit/Productos.java
+++ src/practicagit/Productos.java
@@ -8,7 +8,7 @@ public class Productos {
    public int id_producto;
     public String nombre;
     public double precio;
-    public String marca=;
+    public String marca;
     public String categoria;
     
 }
